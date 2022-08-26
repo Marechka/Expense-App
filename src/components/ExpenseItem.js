28577eb35ab5f9e708373bab70d6c7c66431a22b
Masha <39933741+Marechka@users.noreply.github.com>
2022-08-25 18:39:50 -0700

    ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,21 @@
-function ExpenseItem() {
-    return <h2>Expense Item!</h2>;
- }
+import "./ExpenseItems.css";
 
- export default ExpenseItem;
+function ExpenseItem(props) {
+    const month  = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocleString('en-US', {day: '2-digit'});
+    const year = props.year.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
